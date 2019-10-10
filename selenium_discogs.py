9dd1fb46f2da34476afa7ae504004e30184908d7
root <root@localhost.localdomain>
2019-10-10 11:37:56 -0400

    favourite search

---
+++ selenium_discogs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env/ python
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from time import sleep
+
+browser = webdriver.Firefox(executable_path='./geckodriver')
+
+def start_site(site):
+    browser.maximize_window()
+    browser.get(site)
+    sleep(1)
+
+def search_marketplace(position):
+    browser.find_element_by_id("marketplace-search-form").click()
+    browser.find_element_by_id("marketplace-search-input").send_keys(position)
+    # browser.find_element_by_id("marketplace-search-input").send_keys(Keys.RETURN)
+    browser.find_element_by_xpath("/html/body/div[3]/div[4]/div[4]/form/button").click()
+
+def add_attributes():
+    actual_site = browser.current_url
+    browser.get(actual_site+"&ships_from=Poland&condition=Near+Mint+%28NM+or+M-%29&format=Vinyl")
+
+start_site("https://www.discogs.com/sell/list")
+
+search_marketplace("Chemical Brothers")
+add_attributes()
+
+#
+# browser.close()
+
