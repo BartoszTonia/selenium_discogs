9e38d8d32b214d62424986d543b8396f69467bee
BartoszSamsung <bartosz.tonia@gmail.com>
2019-12-01 20:45:46 +0100

    small edits

--- selenium_discogs.py
+++ selenium_discogs.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env/ python
-
+#! usr/bin/env python
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from time import sleep
@@ -9,30 +8,27 @@ browser = webdriver.Firefox(executable_path='./geckodriver')
 
 parser = argparse.ArgumentParser(description="Wyszukiwanie plyt vinylowych na Discogs")
 parser.add_argument('-v', '--vinyl', dest="vinyl", type=str, help="pass vinyl title")
-args = parser.parse_args()
+
 
 def start_site(site):
     browser.maximize_window()
     browser.get(site)
-    sleep(2)
 
 def search_marketplace(position):
     browser.find_element_by_id("onetrust-accept-btn-handler").click()
-    sleep(2)
     browser.find_element_by_id("marketplace-search-input").send_keys(position)
     browser.find_element_by_id("marketplace-search-input").send_keys(Keys.RETURN)
-    # browser.find_element_by_xpath("/html/body/div[3]/div[4]/div[4]/form/button").click()
 
 def add_attributes():
     actual_site = browser.current_url
     browser.get(actual_site+"&ships_from=Poland&condition=Near+Mint+%28NM+or+M-%29&format=Vinyl")
 
 start_site("https://www.discogs.com/sell/list")
-
-search_marketplace(args.vinyl)
+sleep(2)
+search_marketplace(parser.parse_args().vinyl)
 sleep(3)
 add_attributes()
-#
+
 # sleep(10000)
 # browser.close()
 
